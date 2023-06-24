3547339b99f4e7dc941ec5810bd5365c50993412
dagil02 <dagil02@ucm.es>
2023-06-24 18:27:46 +0200

    inicio deserializacion

---
+++ TelemetryProcess/TelemetryProcess/Event.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telemetry.Events
+{
+    [Serializable]
+    public class Event
+    {
+        public Int32 _eventId;
+        public long _timeStamp;
+        
+        public Dictionary<string, object> _data;
+        public Event(Int32 eventId)
+        {
+            this._eventId = eventId;
+            this._data = new Dictionary<string, object>();
+            _timeStamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+             string evType=((EventType)(_eventId)).ToString(); //casts the enum to string to be used as a description of the event
+            _data.Add("Event", evType);
+            _data.Add("TimeStamp", _timeStamp);
+   
+    }
+        public Int32 getEvent()
+        {
+            return _eventId;
+        }
+        public Dictionary<string, object> getData()
+        {
+            return _data;
+        }
+    }
+}
---
+++ TelemetryProcess/TelemetryProcess/EventType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Telemetry.Events
+{
+    /// <summary>
+    /// All the events are numbered here
+    /// </summary>
+    public enum EventType
+    {
+        //basic events
+        StartTracking,  //
+        StopTracking,   //
+        SessionStart,   //
+        SessionEnd,     //
+        GameStart,      //
+        GameEnd,        //
+        LevelStart,     //
+        LevelEnd,       //
+        GamePaused,     //
+        GameResumed,    //
+       
+        //Wizara Events
+        StartPuzzle,    //
+        EndPuzzle,      //
+        PlayerDamaged,  //
+        PlayerDead,     //
+        ItemDropped,    //
+        ItemPicked,     //
+        AbilityCasted,  //
+        AbilityHit,     //
+
+        //More events here
+        
+        numOfEvents
+    }
+}
